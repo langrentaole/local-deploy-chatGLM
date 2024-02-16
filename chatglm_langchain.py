81294dc9db1a0d7aa5dd2f89daa10dd466fef03a
langrentaole <939768209@qq.com>
2024-02-16 17:14:42 +0800

    code

--- chatglm_langchain.py
+++ chatglm_langchain.py
@@ -3,8 +3,8 @@ from langchain.llms import ChatGLM
 endpoint_url = 'http://127.0.0.1:8000'
 llm = ChatGLM(
     endpoint_url=endpoint_url,
-    max_tokens=80000,
-    top_p=0.9
+    max_tokens=80000,  # 最大传入字符数
+    top_p=0.9  # 控制生成文字的随机性 default 1，between 1 to 2，越大越随机
 )
 while True:
     user_input = input('user:')
